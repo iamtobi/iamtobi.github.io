133dd7723c1d8abc81d53c54a57600ff6ff0f43e
Oluwatobi <88734402+ccipher-lab@users.noreply.github.com>
2022-05-01 20:29:03 +0100

    commit

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-document.querySelector("form").addEventListener("submit", handleSubmit);
+function submitRequest() {
+  document.querySelector("form").addEventListener("submit", handleSubmit);
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  let myForm = document.getElementById("hire-form");
-  let formData = new FormData(myForm);
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error));
-};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let myForm = document.getElementById("hire-form");
+    let formData = new FormData(myForm);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+  return 0;
+}
+submitRequest();
+document.getElementById("sub").innerHTML = "Email";
